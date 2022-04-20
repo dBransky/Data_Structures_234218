c08eb75d8472cb786a685726ccc55e0b7664d72c
itamararbiv <103099905+itamararbiv@users.noreply.github.com>
2022-04-20 23:21:06 +0300

    Add files via upload

--- wet1/HighTech.cpp
+++ wet1/HighTech.cpp
@@ -4,13 +4,14 @@
 
 #include "HighTech.h"
 
+
 HighTech::HighTech() : employees(), companies() {
 
 }
 
-void HighTech::AddEmployee(int employee_id, int company_id, int salary, int grade) {
+void HighTech::AddEmployee(int employee_id, int company_id, int salary, int grade){
     employees.insert(employee_id, Employee(company_id, salary, grade));
-
+    companies.find(company_id).AddNewEmployee();
 }
 
 void HighTech::AddCompany(int company_id, int value) {
@@ -20,6 +21,7 @@ void HighTech::AddCompany(int company_id, int value) {
 
 void HighTech::RemoveEmployee(int employee_id) {
     employees.remove(employee_id);
+    // something wrong here...
 
 }
 
@@ -28,10 +30,100 @@ void HighTech::RemoveCompany(int company_id) {
 
 }
 
-Company::Company(int value) : value(value) {
+void HighTech::GetCompanyInfo(int company_id, int *Value, int *NumEmployees)
+{
+    Company& company = companies.find(company_id);
+    *NumEmployees = company.GetAmountOfEmployees();
+    *Value = company.GetCompanyValue();
+}
 
+void HighTech::GetEmployeeInfo(int EmployeeId, int *EmployerID, int *Salary, int *Grade)
+{
+    Employee& employee = employees.find(EmployeeId);
+    *EmployerID = employee.GetCompanyId();
+    *Salary = employee.GetSalary();
+    *Grade = employee.GetGrade();
+}
+
+void HighTech::IncreaseCompanyValue(int CompanyId, int ValueIncrease)
+{
+    Company& company = companies.find(CompanyId);
+    company.AddCompanyValue(ValueIncrease);
+}
+
+void HighTech::PromoteEmployee(int EmployeeID, int SalaryIncrease, int BumpGrade)
+{
+    Employee& employee = employees.find(EmployeeID);
+    employee.IncreaseSalary(SalaryIncrease);
+    if (BumpGrade > 0)
+    {
+        employee.IncreaseGrade();
+    }
+}
+
+void HighTech::HireEmployee(int EmployeeID, int NewCompanyID)
+{
+    
+}
+
+
+
+
+
+
+
+Company::Company(int value, int amount_of_employees) : value(value), amount_of_employees(amount_of_employees) {
+
+}
+
+void Company::AddCompanyValue(int valueIncrease)
+{
+    value = value + valueIncrease;
+}
+
+int Company::GetCompanyValue()
+{
+    return value;
+}
+
+int Company::GetAmountOfEmployees()
+{
+    return amount_of_employees;
+}
+
+void Company::AddNewEmployee() {
+    amount_of_employees++;
+}
+
+void Company::RemoveEmployee() {
+    amount_of_employees--;
 }
 
 Employee::Employee(int salary, int grade, int company_id) : salary(salary), grade(grade), company_id(company_id) {
 
+}
+
+int Employee::GetSalary()
+{
+    return salary;
+}
+
+void Employee::IncreaseSalary(int valueIncrease)
+{
+    salary = salary + valueIncrease;
+}
+
+void Employee::IncreaseGrade()
+{
+    grade = grade + 1;
+}
+
+int Employee::GetGrade()
+{
+    return grade;
+}
+
+int Employee::GetCompanyId()
+{
+    return company_id;
 }
--- wet1/HighTech.h
+++ wet1/HighTech.h
@@ -14,13 +14,24 @@ private:
     int company_id;
 public:
     Employee(int salary, int grade, int company_id);
+    int GetSalary();
+    int GetGrade();
+    int GetCompanyId();
+    void IncreaseSalary(int valueIncrease);
+    void IncreaseGrade();
 };
 
 class Company {
 private:
     int value;
+    int amount_of_employees;
 public:
-    Company(int);
+    Company(int value, int amount_of_employees = 0);
+    void AddCompanyValue(int valueIncrease);
+    void AddNewEmployee();
+    void RemoveEmployee();
+    int GetCompanyValue();
+    int GetAmountOfEmployees();
 };
 
 class HighTech {
@@ -29,15 +40,35 @@ class HighTech {
 public:
     HighTech();
 
-    void AddEmployee(int employee_id, int company_id, int salary, int grade);
+    void AddEmployee(int employee_id, int company_id, int salary, int grade); // v
 
-    void RemoveEmployee(int employee_id);
+    void RemoveEmployee(int employee_id); // v
 
-    void RemoveCompany(int company_id);
+    void RemoveCompany(int company_id); // v
 
-    void AddCompany(int company_id, int value);
+    void AddCompany(int company_id, int value); // v
 
+    void GetCompanyInfo(int company_id, int *Value, int *NumEmployees); // v
 
+    void GetEmployeeInfo(int EmployeeId, int *EmployerID, int *Salary, int *Grade); // v
+
+    void IncreaseCompanyValue(int CompanyId, int ValueIncrease); // v
+
+    void PromoteEmployee(int EmployeeID, int SalaryIncrease, int BumpGrade); // v
+
+    void HireEmployee(int EmployeeID, int NewCompanyID);
+
+    void AcquireCompany(int AcquireID, int TargetID, double Factor);
+
+    void GetHighestEarner(int CompanyID, int *EmployeeID);
+
+    void GetAllEmployeesBySalary(int companyID, int **Employees, int *NumOfEmployees);
+
+    void GetHighestEarnerInEachCompany(int NumOfCompanies,int **Employees);
+
+    void GetNumEmployeesMatching(int CompanyID, int MinEmployeeID, int MaxEmployeeID, int MinSalary, int MinGrade, int *TotalNumOfEmployees, int *NumOfEmployees);
+
+    void Quit();
 };
 
 #endif //DATA_STRUCTURES_234218_HIGHTECH_H
